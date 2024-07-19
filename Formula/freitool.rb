b261910552b8d84a3b08d063426c765efb4d4f4b
Anderson Freitas <freitas@disroot.org>
2024-07-19 07:47:33 +0000

    Bump version

--- Formula/freitool.rb
+++ Formula/freitool.rb
@@ -1,15 +1,15 @@
 class Freitool < Formula
   desc "Freitool"
   homepage "https://github.com/andersonfds/freitool"
-  version "{{VERSION}}"
+  version "v0.1.0-alpha.3"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/andersonfds/freitool/releases/download/vv0.1.0-alpha.3/freitool_macOS_aarch64-apple-darwin.tar.gz"
-      sha256 "{{SHA256_ARM}}"
+      sha256 "90b060dfdd9cb17cbf2f1d5f3be516491f23ea765228d49340167f76a89e1a60"
     elsif Hardware::CPU.intel?
       url "https://github.com/andersonfds/freitool/releases/download/vv0.1.0-alpha.3/freitool_macOS_x86_64-apple-darwin.tar.gz"
-      sha256 "{{SHA256_INTEL}}"
+      sha256 "c375e89b0b17e635293e0d6e8321fe905ee18751250f6f4355aa4af5fcb052c2"
     end
   end
 
