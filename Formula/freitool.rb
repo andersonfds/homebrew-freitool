892a464ef29d5434efdd68a60b3aa47f4994f36f
Anderson <freitas@disroot.org>
2024-07-19 10:02:30 +0300

    Update freitool.rb

--- Formula/freitool.rb
+++ Formula/freitool.rb
@@ -1,15 +1,15 @@
 class Freitool < Formula
   desc "Freitool"
   homepage "https://github.com/andersonfds/freitool"
-  version "0.1.0"
+  version "0.1.0-alpha.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/andersonfds/freitool/releases/download/v0.1.0/freitool_macOS_arm.tar.gz"
-      sha256 "e57595408ead35c6e8c6e414c4280941dc964bf6fa057e9b3519c651a94f5d4b"
+      url "https://github.com/andersonfds/freitool/releases/download/v0.1.0-alpha.2/freitool_macOS_arm.tar.gz"
+      sha256 "0b05fdeb775a5b930d4266bc9ac262bb5b6073350d038240344415a47d93c23a"
     elsif Hardware::CPU.intel?
-      url "https://github.com/andersonfds/freitool/releases/download/v0.1.0/freitool_macOS_x86.tar.gz"
-      sha256 "e6168a7867eb27af099c44acbe6123ccd763cafb7ee1729c698927e54c6a4b3f"
+      url "https://github.com/andersonfds/freitool/releases/download/v0.1.0-alpha.2/freitool_macOS_x86.tar.gz"
+      sha256 "0d05035ab598a1022ca5bb75268ea6a7bddb594a1cb4b75b103ffe6415fc7f3b"
     end
   end
 
