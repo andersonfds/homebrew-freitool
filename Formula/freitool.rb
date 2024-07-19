3dca4fe9e7eec26fcdbbf5701f2fffcc7d2abbd5
Anderson <freitas@disroot.org>
2024-07-19 10:02:48 +0300

    Update freitool.rb download file was wrong

--- Formula/freitool.rb
+++ Formula/freitool.rb
@@ -5,10 +5,10 @@ class Freitool < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/andersonfds/freitool/releases/download/v0.1.0-alpha.2/freitool_macOS_arm.tar.gz"
+      url "https://github.com/andersonfds/freitool/releases/download/v0.1.0-alpha.2/freitool_macOS_aarch64-apple-darwin.tar.gz"
       sha256 "0b05fdeb775a5b930d4266bc9ac262bb5b6073350d038240344415a47d93c23a"
     elsif Hardware::CPU.intel?
-      url "https://github.com/andersonfds/freitool/releases/download/v0.1.0-alpha.2/freitool_macOS_x86.tar.gz"
+      url "https://github.com/andersonfds/freitool/releases/download/v0.1.0-alpha.2/freitool_macOS_x86_64-apple-darwin.tar.gz"
       sha256 "0d05035ab598a1022ca5bb75268ea6a7bddb594a1cb4b75b103ffe6415fc7f3b"
     end
   end
